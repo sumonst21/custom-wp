2c94dad5a23010fe2dad868b215cad72bb2312e6
Tim Wheeler <tim@timwheeler.com>
2018-09-30 22:48:53 -0400

    added menu links

--- app/public/wp-content/themes/careerdevs-theme/functions.php
+++ app/public/wp-content/themes/careerdevs-theme/functions.php
@@ -8,3 +8,9 @@
     }
 
     add_action('wp_enqueue_scripts', 'careerdevs_files');
+
+    function careerdevs_features() {
+        add_theme_support('title-tag');
+    }
+
+    add_action('after_setup_theme', 'careerdevs_features');
